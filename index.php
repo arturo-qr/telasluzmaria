93ae560c95b98fc7f3e62c2be7ffecaa64ed8185
Arturo QR <arturo.qramirez@gmail.com>
2013-07-26 13:07:45 -0700

    intro

---
+++ index.php
@@ -0,0 +1,41 @@
+
+<html>
+<head>
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="screen">
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+	<link rel="icon" href="images/favicon.ico" type="image/ico" />
+	<title>Telas Luz Maria</title>
+</head>
+<body>
+	<div id="pagecontainer">
+		<div id="header">
+			<div class="logo"></div>
+			<div class="telasluzmaria"></div>
+			<div id="anuncio">
+				<h1>Bajo Construcci&oacute;n</h1>
+			</div>
+		</div>
+		<div class="division"></div>
+		<div id="page">
+			<div class="division2"></div>
+			<!--Imagen Boton-->
+			<div id="default-images" class="primera"></div>
+			<!--Imagen Ziper-->
+			<div id="default-images" class="segunda"></div>
+			<!--Imagen Flores-->
+			<div id="default-images" class="tercera"></div>
+			<!--Imagen Alfileres-->
+			<div id="default-images" class="cuarta"></div>
+			<!--Anuncio Telas-->
+			<div id="tmalta" class="telas"></div>
+			<!--Anuncio Alta Costura-->
+			<div id="tmalta" class="costura"></div>
+			<!--Anuncio Merceria-->
+			<div id="tmalta" class="merceria"></div>
+		</div>
+		<div class="clear"></div>
+	</div>
+</body>
+<div id="footer"></div>
+
+</html>
