17754a5301ebf74f1f7b363a2cb93c856a0c8655
Rocandio <juanrocandio@hotmail.com>
2013-07-31 12:58:11 -0700

    Fade delay

--- js/custom.js
+++ js/custom.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 	$("#pagecontainer").css("display", "none");
 	$("#zipper").css("display", "none");
 	
-    $("#pagecontainer").fadeIn(100);
+    $("#pagecontainer").fadeIn(500);
 	$("a.transition").click(function(event){
 		
 		event.preventDefault();
 		linkLocation = this.href;
 		
-		$("#pagecontainer").fadeOut(100,redirectPage);
+		$("#pagecontainer").fadeOut(500,redirectPage);
 		
 	});
 		
